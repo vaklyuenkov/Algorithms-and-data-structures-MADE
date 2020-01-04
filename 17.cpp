05e49649195c026f092f28cbcb446d8900b733e0
vaklyuenkov <vaklyuenkov@gmail.com>
2020-01-04 23:26:34 +0300

    fixes 17 task

--- 17.cpp
+++ 17.cpp
@@ -36,59 +36,57 @@ stdout:
 #include <vector>
 
 
-struct BorNode
-    {
-    // we will store all nodes in vector. Is it necessary to rewrite with pointers?
-        explicit BorNode(int alphabetSize) : IsTerminal(false), Parent(0), StateByChar(0), SuffixLink(-1), UpSuffixsLink(-1)
-        {
-            NextStates.assign(alphabetSize, -1);
-            DeltaFunction.assign(alphabetSize, -1);
-        };
-        bool IsTerminal;
-        int Parent;
-        int StateByChar;
-        int SuffixLink;
-        int UpSuffixsLink;
-        std::vector<int> NextStates;
-        std::vector<int> DeltaFunction;
-        std::vector<int> Lines;
-    };
-
 class Trie
     {
         public:
-            Trie();
+            Trie(const std::string & pattern);
             ~Trie() = default; // we dont need to delete our tree in destructor if store in vector
             Trie(const Trie&) = delete; // according to five-rule
             Trie(Trie&&) = delete;
             Trie& operator=(const Trie&) = delete;
             Trie& operator=(Trie&&) = delete;
 
-            void build(const std::string & pattern);
-            void find(const std::string& text);
-            std::vector<int> getAnswer();
+            std::vector<int>  find(const std::string& text, std::vector<int> &result) const;
 
         private:
+            struct BorNode
+            {
+                explicit BorNode(int alphabetSize) : IsTerminal(false), Parent(0), StateByChar(0), SuffixLink(-1), UpSuffixsLink(-1)
+                {
+                    NextStates.assign(alphabetSize, -1);
+                    DeltaFunction.assign(alphabetSize, -1);
+                };
+                bool IsTerminal;
+                int Parent;
+                int StateByChar;
+                int SuffixLink;
+                int UpSuffixsLink;
+                std::vector<int> NextStates;
+                std::vector<int> DeltaFunction;
+                std::vector<int> Lines;
+            };
             const int  alphabetSize = 26;
             int patternSize;
             unsigned int lastState;
             std::vector <std::string> string_vector;
             std::vector <int> starts;
             std::vector<BorNode> nodes;
-            std::vector<int> answer;
 
+
+            void build(const std::string & pattern);
             void preprocessPattern(const std::string & pattern);
             void addString(const std::string & string, int index);
-            int getSuffixLink(int state);
-            int getUpLink(int state);
-            int getDelta(int state, int stateByChar);
+            int getSuffixLink(int state) const;
+            int getUpLink(int state) const;
+            int getDelta(int state, int stateByChar) const;
     };
 
-Trie::Trie()
+Trie::Trie(const std::string & pattern)
     {
         BorNode root(alphabetSize);
         nodes.push_back(root);
         lastState = 1;
+        build(pattern);
     }
 
 void Trie::build(const std::string & pattern)
@@ -101,7 +99,7 @@ void Trie::build(const std::string & pattern)
         }
     }
 
-void Trie::find(const std::string &text)
+std::vector<int> Trie::find(const std::string &text, std::vector<int> &result) const
     {
         int position = 0;
         std::vector<int> solver(text.length());
@@ -131,8 +129,10 @@ void Trie::find(const std::string &text)
         for (int j = 0; j < solver.size() - patternSize + 1; ++j)
         {
             if (solver[j] == string_vector.size())
-                answer.push_back(j);
+                result.push_back(j);
         }
+
+    return result;
     }
 
 void Trie::preprocessPattern(const std::string &pattern)
@@ -180,64 +180,62 @@ void Trie::addString(const std::string &string, int index)
         nodes[state].Lines.push_back(starts[index]);
     }
 
-int Trie::getSuffixLink(int state)
+int Trie::getSuffixLink(int state) const
     {
+        int stateSuffixLink = 0;
         if (nodes[state].SuffixLink == -1)
         {
             int par = nodes[state].Parent, sbc = nodes[state].StateByChar;
             if (state == 0 || par == 0)
             {
-                nodes[state].SuffixLink = 0;
+                stateSuffixLink = 0;
             }
             else
             {
-                nodes[state].SuffixLink = getDelta(getSuffixLink(par), sbc);
+                stateSuffixLink = getDelta(getSuffixLink(par), sbc);
             }
         }
-        return nodes[state].SuffixLink;
+        return stateSuffixLink;
     }
 
-int Trie::getDelta(int state, int stateByChar)
+int Trie::getDelta(int state, int stateByChar) const
     {
+        int stateByCharDeltaFunction = 0;
         if (nodes[state].DeltaFunction[stateByChar] == -1)
             if (nodes[state].NextStates[stateByChar] != -1)
             {
-                nodes[state].DeltaFunction[stateByChar] = nodes[state].NextStates[stateByChar];
+                stateByCharDeltaFunction = nodes[state].NextStates[stateByChar];
             }
         else
             {
-                nodes[state].DeltaFunction[stateByChar] = (state == 0) ? 0 : getDelta(getSuffixLink(state), stateByChar);
+                stateByCharDeltaFunction = (state == 0) ? 0 : getDelta(getSuffixLink(state), stateByChar);
             }
 
-        return nodes[state].DeltaFunction[stateByChar];
+        return stateByCharDeltaFunction;
     }
 
-int Trie::getUpLink(int state)
+int Trie::getUpLink(int state) const
     {
+        int stateUpSuffixsLink = 0;
         if (nodes[state].UpSuffixsLink == -1)
         {
             int currentState = getSuffixLink(state);
             if (nodes[currentState].IsTerminal)
             {
-                nodes[state].UpSuffixsLink = currentState;
+                stateUpSuffixsLink = currentState;
             }
             else
             {
-                nodes[state].UpSuffixsLink = currentState == 0 ? 0 : getUpLink(currentState);
+                stateUpSuffixsLink = currentState == 0 ? 0 : getUpLink(currentState);
             }
         }
 
-        return  nodes[state].UpSuffixsLink;
-    }
-
-std::vector<int> Trie::getAnswer()
-    {
-        return answer;
+        return  stateUpSuffixsLink;
     }
 
 int main()
     {
-        Trie trie;
+
         std::string pattern;
         std::cin >> pattern;
         std::string text;
@@ -248,9 +246,8 @@ int main()
             return 0;
         }
         std::vector <int> result;
-        trie.build(pattern);
-        trie.find(text);
-        result = trie.getAnswer();
+        Trie trie(pattern);
+        result = trie.find(text, result);
         for (auto position: result)
         {
             std::cout << position << " ";
