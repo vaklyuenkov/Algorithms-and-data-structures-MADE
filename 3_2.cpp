e38dab379c0e59205c3e70ebaaaffecb6daee160
vaklyuenkov <vaklyuenkov@gmail.com>
2020-01-04 12:35:21 +0300

    3_2 fixes

--- 3_2.cpp
+++ 3_2.cpp
@@ -11,181 +11,190 @@
 Последовательность может быть очень длинной.
 Время работы O(n * log(k)). Доп. память O(k). Использовать слияние.
 
+In:
+
+10 4
+0 4 3 2 1 8 7 6 5 9
+
+Out:
+
+0 1 2 3 4 5 6 7 8 9
+
+
 */
 #include <iostream>
+#include <vector>
 
 // to sort we will use heap functions from my previous task
 int getParentPosition(int position)
-{
-    return (position-1)/2;
-}
+    {
+        return (position-1)/2;
+    }
 
 int getLefChildPosition(int position)
-{
-    return (2*position+1);
-}
+    {
+        return (2*position+1);
+    }
 
 int getRightChildPosition(int position)
-{
-    return (2*position+2);
-}
-
-void siftUp(int *array, int position)
-{
-    while (position > 0 && array[getParentPosition(position)] < array[position])
     {
-        std::swap(array[position], array[getParentPosition(position)]);
-        position = getParentPosition(position);
+        return (2*position+2);
     }
-}
 
-void siftDown(int *heapArray, int heapSize,  int position)
-{
-    while(position*2+1 <= heapSize-1)  // check that current position could have at least one child
+template <class T, class TLess>
+void siftDown(T *heapArray, int heapSize,  int position, TLess comparator)
     {
-        int lefChildPosition = getLefChildPosition(position);
-        int rightChildPosition = getRightChildPosition(position);
-        int positionOfMaximum = position;
-
-        if (lefChildPosition <= heapSize - 1 && heapArray[lefChildPosition] > heapArray[position])
-            positionOfMaximum = lefChildPosition;
-
-        if (rightChildPosition <= heapSize - 1 && heapArray[rightChildPosition] > heapArray[positionOfMaximum])
-            positionOfMaximum = rightChildPosition;
-
-        if (position != positionOfMaximum)
+        while(position*2+1 <= heapSize-1)  // check that current position could have at least one child
         {
-            std::swap(heapArray[position], heapArray[positionOfMaximum]);
-            position = positionOfMaximum;
-        }
-        else break;
-    }
-}
+            int lefChildPosition = getLefChildPosition(position);
+            int rightChildPosition = getRightChildPosition(position);
+            int positionOfMaximum = position;
 
-void push(int *array, int heap_size, int value)  //to build heap adding values, like we did in previous task
-{
-    array[heap_size - 1] = value;
-    siftUp(array, heap_size - 1);
-}
+            if (lefChildPosition <= heapSize - 1 && comparator(heapArray[position], heapArray[lefChildPosition]))
+                positionOfMaximum = lefChildPosition;
 
-void sort(int *array, int arraySize) //to sort by heap
-{
-    for (int i=arraySize-1; i>=0; i--)
-    {
-        std::swap(array[0], array[i]);
-        siftDown(array, i, 0);
-    }
-}
+            if (rightChildPosition <= heapSize - 1 && comparator(heapArray[positionOfMaximum],  heapArray[rightChildPosition]))
+                positionOfMaximum = rightChildPosition;
 
-void merge (const int *firstArray, const int *secondArray, int *result, int firstArraySize, int secondArraySize)
-{
-    int i=0; //index of element to compare in first array
-    int j=0; //index of element to compare in second array
-
-    while (i != firstArraySize && j != secondArraySize) // to check not all values was taken from one of arrays
-    {
-        if (firstArray[i] < secondArray[j])
-        {
-            result[i+j]=firstArray[i];
-            i++;
-        }
-
-        else
-        {
-            result[i+j]=secondArray[j];
-            j++;
-        }
-    }
-
-    // copy the rest of the the remaining array into the result according to the lecture
-    if (i == firstArraySize)
-    {
-        for (; j < secondArraySize; j++)
-        {
-            result[i+j]=secondArray[j];
-        }
-
-    }
-
-    else if (j == secondArraySize)
-    {
-        for (; i < firstArraySize; i++)
-        {
-            result[i+j]=firstArray[i];
-        }
-    }
-}
-
-void print(int *array, int startPosition, int endPosition)
-{
-    for (int i=startPosition; i < endPosition; i++)
-    {
-        std::cout<<array[i]<<" ";
-    }
-}
-
-int main ()
-{
-    int inputSize = 0; // length of input array
-    int offset = 0;    // k
-
-    std::cin >> inputSize >> offset;
-
-//    we can use this way if we are sure than offset isn`t very big. In This case we dont need `delete [] firstArray;`.
-//    int firstArray[offset];
-//    int secondArray[offset];
-//    int mergedArray[2 * offset];
-
-//  this way to define arrays will be twice as slower compared to `int firstArray[offset];`
-//  with the same memory allocation
-//  but 2*offset may be huge, so we will use dynamic array
-    int* firstArray = new int[offset];
-    int* secondArray = new int[offset];
-    int* mergedArray = new int[2 * offset]; // we allocate memory in sum 4*k == O(k)
-
-    int globalCounter = 0; // to store len of all data we already read
-    int localCounter = 0;  // to store len of data we read in current array
-
-    while (globalCounter < inputSize)
-    {
-        if (globalCounter == 0)
-        {
-            for (int i=0; i < offset;  i++)
+            if (position != positionOfMaximum)
             {
-                int value;
-                std::cin >> value;
+                std::swap(heapArray[position], heapArray[positionOfMaximum]);
+                position = positionOfMaximum;
+            }
+            else break;
+        }
+    }
+
+template <class T, class TLess>
+void heapSort(T *array, int arraySize, TLess comparator)
+    {
+        for (int i = getParentPosition(arraySize - 1); i >= 0; i--)  //first build heap
+        {
+            siftDown(array, arraySize, i, comparator);
+        }
+        for (int i=arraySize-1; i>=0; i--)  // second - sort
+        {
+            std::swap(array[0], array[i]); // Move current root to end
+            siftDown(array, i, 0,  comparator); //heapify
+        }
+    }
+
+template <class T>
+void merge (const T *firstArray, const T *secondArray, T *result, int firstArraySize, int secondArraySize)
+    {
+        int i=0; //index of element to compare in first array
+        int j=0; //index of element to compare in second array
+
+        while (i != firstArraySize && j != secondArraySize) // to check not all values was taken from one of arrays
+        {
+            if (firstArray[i] < secondArray[j])
+            {
+                result[i+j]=firstArray[i];
+                i++;
+            }
+
+            else
+            {
+                result[i+j]=secondArray[j];
+                j++;
+            }
+        }
+
+        // copy the rest of the the remaining array into the result according to the lecture
+        if (i == firstArraySize)
+        {
+            for (; j < secondArraySize; j++)
+            {
+                result[i+j]=secondArray[j];
+            }
+
+        }
+
+        else if (j == secondArraySize)
+        {
+            for (; i < firstArraySize; i++)
+            {
+                result[i+j]=firstArray[i];
+            }
+        }
+    }
+
+template <class T, class TLess>
+std::vector <T> sortAlmostSorted(T *inputArray, int offset, int inputSize, TLess comparator)
+    {
+        T* firstArray = new int[offset];
+        T* secondArray = new int[offset];
+        T* mergedArray = new int[2 * offset]; // we allocate memory in sum 4*k == O(k)
+        std::vector <T> answer;
+
+        int globalCounter = 0; // to store len of all data we already read
+        int localCounter = 0;  // to store len of data we read in current array
+
+        while (globalCounter < inputSize)
+        {
+            if (globalCounter == 0)
+            {
+                for (int i=0; i < offset;  i++)
+                {
+                    firstArray[i] = inputArray[i];
+                    localCounter+=1;
+                    globalCounter+=1;
+                }
+                heapSort(firstArray, offset,comparator); // heap sort
+            }
+
+            localCounter=0;
+            for (int i=0; i < offset && globalCounter < inputSize;  i++)
+            {
+                secondArray[i] = inputArray[globalCounter];
                 localCounter+=1;
-                push(firstArray, localCounter, value); //build heap
                 globalCounter+=1;
             }
-            sort(firstArray, offset); // heap sort
+
+            heapSort(secondArray, localCounter, comparator); // heap sort
+            merge(firstArray, secondArray, mergedArray, offset, localCounter);
+
+            for (int i=0; i < offset; i++) //we can be sure only in first half of merged array
+            {
+                answer.push_back(mergedArray[i]);
+            }
+
+            for (int i=0; i < offset;  i++) //move second half of merged array to firsArray to merge with next batch of data
+            {
+                firstArray[i]=mergedArray[i + offset];
+            }
         }
 
-        localCounter=0;
-        for (int i=0; i < offset && globalCounter < inputSize;  i++)
+        for (int i=offset; i < offset + localCounter; i++) //print the rest
         {
-            int value;
-            std::cin >> value;
-            localCounter+=1;
-            push(secondArray, localCounter, value);  //build heap
-            globalCounter+=1;
+            answer.push_back(mergedArray[i]);
         }
 
-        sort(secondArray, localCounter); // heap sort
-        merge(firstArray, secondArray, mergedArray, offset, localCounter);
-        print(mergedArray, 0, offset); //we can be sure only in first half of merged array
+        delete [] firstArray;
+        delete [] secondArray;
+        delete [] mergedArray;
 
-        for (int i=0; i < offset;  i++) //move second half of merged array to firsArray to merge with next batch of data
-        {
-            firstArray[i]=mergedArray[i + offset];
-        }
+        return answer;
     }
 
-    print(mergedArray, offset, offset + localCounter); //print the rest
+int main ()
+    {
+        int inputSize = 0; // length of input array
+        int offset = 0;    // k
 
-    delete [] firstArray;
-    delete [] secondArray;
-    delete [] mergedArray;
+        std::cin >> inputSize >> offset;
+        int* input = new int[inputSize];
+        for (int i = 0; i < inputSize; i++)
+        {
+            std::cin >> input[i];
+        }
+        std::vector <int> answer;
+        answer = sortAlmostSorted(input, offset, inputSize, std::less<int>());
+        for (auto element: answer)
+        {
+            std::cout << element << " ";
+        }
 
-    return 0;
-}
+        delete [] input;
+        return 0;
+    }
